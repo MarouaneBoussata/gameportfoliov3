71c0289ec860beb3f5afeacb70f6d3f1999f8e63
MarouaneBoussata <marwaneboussata@gmail.com>
2024-07-31 05:39:29 +0100

    add22qdguydffd

--- src/exit/ExitMenu.js
+++ src/exit/ExitMenu.js
@@ -5,15 +5,18 @@ import { useNavigate } from 'react-router-dom'
 const ExitMenu = () => {
 
     const Navigate1 = (evt) => {
-        Navigate2('https://mb6devops.netlify.app/')
+        Navigate2('/')
     }
     const Navigate2 = useNavigate()
-    
+    const handleRedirect = () => {
+      window.location.href = 'https://mb6devops.netlify.app/';
+    };
+  
 
   return (
     <div className='center'>
         <p className='pExit'>Do you want to exit ?</p>
-            <button  className='button1' onClick={Navigate1}> YES </button>
+           <button  className='button1' onClick={handleRedirect}>  YES </button> 
             <button className='button2' onClick={()=>Navigate1}>NO</button>
     </div>
   )
